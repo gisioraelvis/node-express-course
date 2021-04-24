3d5e44befccba15ef8e6b44fe80853764ce87524
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-04-24 09:44:47 +0300

    Added route to get one user

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get("/users", (req, res) => {
   });
 });
 
+app.get('/users/:id',(req,res)=>{
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
 app.listen(PORT, () => {
   console.log(`server running on localhost:${PORT}`);
 });
