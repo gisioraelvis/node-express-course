d9c9a78436f738b3b8abbf2628e54dac5d74e074
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-04-24 09:19:10 +0300

    Added first route

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+
+const mockUsersData = [{ name: "Elvis" }, { name: "Oay" }];
+
+app.get("/users", (req, res) => {
+  res.json({
+    success: true,
+    message: "User retrival success",
+    users: mockUsersData,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`server running on localhost:${PORT}`);
 });
