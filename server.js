f6a41a76b093cdf89a8660e20fcce62d4a08cf03
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-04-24 10:00:16 +0300

    Added mock login

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const PORT = 8000;
+
+
+app.use(bodyParser.json())
 
 const mockUsersData = [{ name: "Elvis" }, { name: "Oay" }];
 
@@ -19,6 +23,27 @@ app.get('/users/:id',(req,res)=>{
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+app.post('/login', (req,res)=>{
+const userName = req.params.userName
+const password = req.params.password
+
+const mockUserName = 'Elithedev',
+const mockPassword = 'supersecret'
+
+if (userName === mockUserName && password === mockPassword){
+  res.json({
+    success: true,
+    message: 'Login Successful',
+    token: 'token goes here'
+  })
+}else{
+  res.json({
+    success: false,
+    message: 'username or password doesnt match'
+  })
+}
 })
 
 app.listen(PORT, () => {
